1d43cb368295eee8a8da53d866ed05bd9f4884e7
unknown <monicaimbrea7@gmail.com>
2024-01-22 20:57:12 +0200

    added About page

---
+++ src/components/pages/About.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+function About() {
+  return (
+    <section class="bg-dark text-light p-5 text-center">
+      <div id="about" class="container">
+        <div class="d-sm-flex justify-content-around align-items-center">
+          <div>
+            <h1 className="d-flex justify-content-start py-4">About infoDEx</h1>
+            <p class="text-start lead w-50">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Distinctio totam ipsum aspernatur modi recusandae reiciendis
+              officia doloribus laborum consequatur maxime molestiae, id ullam
+              quidem aperiam harum minus dolor sint vel nulla eos dicta! Aliquam
+              optio quidem ad ex adipisci eveniet magnam odit numquam, excepturi
+              ipsam sunt beatae eaque nesciunt quaerat officia quis error nobis
+              dolores, neque veritatis eos atque. Nihil asperiores culpa
+              pariatur exercitationem sed quos, ipsa illum, enim unde aliquam
+              dignissimos eius hic dicta itaque, dolor nisi! Quasi accusamus
+              laborum voluptate facilis tempore mollitia, totam blanditiis
+              sapiente quae, non debitis odio. Doloremque excepturi voluptate ad
+              aliquid quam accusamus aut.
+            </p>
+            <div class="icon mb-4"></div>
+          </div>
+          <img class="img-fluid w-25 rounded" src="" alt="" />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default About;
