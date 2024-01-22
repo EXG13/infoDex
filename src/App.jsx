46e1ad76d7068eb4c05635283fe8d84df3f5eeb9
Tomer J. Lind <digitalassetshub@gmail.com>
2024-01-22 21:28:36 +0200

    removed newsboard component

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,12 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import NewsBoard from "./components/NewsBord";
 import About from "./components/pages/About";
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <NewsBoard />
 
       <Routes>
         <Route path="about" element={<About />} />
