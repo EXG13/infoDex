22cb77ec3365eb2af51ca374387c515e067e386b
Tomer J. Lind <digitalassetshub@gmail.com>
2024-01-22 21:36:41 +0200

    new assets path

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import logo from "../assets/logo.png";
 import { NavLink } from "react-router-dom";
 import Coins from "./Coins";
+// import logo from "../assets/logo.png";
+import logo from "/Public/images/logo.png";
 
 function Navbar() {
   return (
