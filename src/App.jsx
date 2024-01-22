025462787374ef39e7bd57cb2f85a7eaef83afd9
unknown <monicaimbrea7@gmail.com>
2024-01-22 20:55:01 +0200

    added navbar component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import NewsBoard from "./components/NewsBord";
+import About from "./components/pages/About";
 
 function App() {
-
   return (
+    <Router>
+      <Navbar />
+      <NewsBoard />
 
-  )
+      <Routes>
+        <Route path="about" element={<About />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
