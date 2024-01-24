44100a9ed378ef0b4338357b89c661e2e1b4be6e
raplordrds <habib.hakeem@yahoo.com>
2024-01-24 19:58:16 +0000

    added social media icons to footer and changed font awesome cdn to version 5.15.0

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,11 +1,68 @@
-import React from 'react'
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import * as bootstrap from "bootstrap";
 
+function footer() {
+    return (
+        <footer>
+            <div className="container-fluid">
+                <div className="row bg-secondary text-white">
+                    <section class="mb-2 mt-2 text-center">
+                        {/* <!-- Facebook --> */}
+                        <a
+                            className="btn btn-outline-light btn-floating m-1"
+                            href="#!"
+                            role="button"
+                        ><i className="fab fa-facebook-f"></i
+                        ></a>
 
-function footer(){
-    return <div className="text-center p-3" style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}>
-            © 2023 Copyright: infoDex
-        </div>
+                        {/* <!-- Twitter --> */}
+                        <a
+                            className="btn btn-outline-light btn-floating m-1"
+                            href="#!"
+                            role="button"
+                        ><i className="fab fa-twitter"></i
+                        ></a>
+
+                        {/* <!-- Google --> */}
+                        <a
+                            className="btn btn-outline-light btn-floating m-1"
+                            href="#!"
+                            role="button"
+                        ><i className="fab fa-google"></i
+                        ></a>
+
+                        {/* <!-- Instagram --> */}
+                        <a
+                            className="btn btn-outline-light btn-floating m-1"
+                            href="#!"
+                            role="button"
+                        ><i className="fab fa-instagram"></i
+                        ></a>
+
+                        {/* <!-- Linkedin --> */}
+                        <a
+                            className="btn btn-outline-light btn-floating m-1"
+                            href="#!"
+                            role="button"
+                        ><i class="fab fa-linkedin-in"></i
+                        ></a>
+
+                        {/* <!-- Github --> */}
+                        <a
+                            className="btn btn-outline-light btn-floating m-1"
+                            href="#!"
+                            role="button"
+                        ><i className="fab fa-github"></i
+                        ></a>
+                    </section>
+                </div>
+
+            </div>
+            <div className="text-center bg-warning fw-semibold p-3">
+                © 2024 Copyright: infoDex
+            </div>
+        </footer>
+    );
 }
 export default footer;
