6cb5d924ef8b9ae51e0c675a9b187b222f8a5c26
unknown <monicaimbrea7@gmail.com>
2024-01-25 20:19:57 +0200

    added images to about and styling

--- src/components/NewsBoard.jsx
+++ src/components/NewsBoard.jsx
@@ -1,22 +1,17 @@
-import { useState } from 'react';
-import Searchbar from "./pages/Searchbar"
+import { useState } from "react";
+import Searchbar from "./pages/Searchbar";
 
 function NewsBoard() {
-  const[searchQuery, setSearchQuery] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
   const handleSearch = (query) => {
-
     console.log("Search Query: ", query);
     setSearchQuery(query);
   };
 
-
-
   return (
-    <div className="board d-flex justify-content-center align-items-center ">
-      <h2 id="title" className="ms-5 text-center">
-        Daily Crypto News
-      </h2>
-      <Searchbar onSearch = {handleSearch}/>
+    <div className="bg-warning d-flex justify-content-center align-items-center ">
+      <h2 className="ms-5 text-center">Daily Crypto News</h2>
+      <Searchbar onSearch={handleSearch} />
     </div>
   );
 }
--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,57 +1,56 @@
 import React from "react";
 import OurTeam from "..//../team.json";
+import about from "..//../assets/about.png";
 
 function About() {
   return (
-    <section className="bg-dark text-light p-5">
-      <div id="about" className="container">
-        <div class="d-sm-flex justify-content-around align-items-center">
-          <div>
-            <h1 className="d-flex justify-content-start py-4">About infoDEx</h1>
-            <p className="text-start lead w-50">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Distinctio totam ipsum aspernatur modi recusandae reiciendis
-              officia doloribus laborum consequatur maxime molestiae, id ullam
-              quidem aperiam harum minus dolor sint vel nulla eos dicta! Aliquam
-              optio quidem ad ex adipisci eveniet magnam odit numquam, excepturi
-              ipsam sunt beatae eaque nesciunt quaerat officia quis error nobis
-              dolores, neque veritatis eos atque. Nihil asperiores culpa
-              pariatur exercitationem sed quos, ipsa illum, enim unde aliquam
-              dignissimos eius hic dicta itaque, dolor nisi! Quasi accusamus
-              laborum voluptate facilis tempore mollitia, totam blanditiis
-              sapiente quae, non debitis odio. Doloremque excepturi voluptate ad
-              aliquid quam accusamus aut.
-            </p>
-            <div className="icon mb-4"></div>
-          </div>
-          <img className="img-fluid w-25 rounded" src="" alt="" />
+    <section className="bg-dark text-light p-4">
+      <div className="container d-sm-flex justify-content-around align-items-center">
+        <div className="container ms-5">
+          <h1 className="py-5">About infoDEx</h1>
+          <p className="lead">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+            totam ipsum aspernatur modi recusandae reiciendis officia doloribus
+            laborum consequatur maxime molestiae, id ullam quidem aperiam harum
+            minus dolor sint vel nulla eos dicta! Aliquam optio quidem ad ex
+            adipisci eveniet magnam odit numquam, excepturi ipsam sunt beatae
+            eaque nesciunt quaerat officia quis error nobis dolores, neque
+            veritatis eos atque. Nihil asperiores culpa pariatur exercitationem
+            sed quos, ipsa illum, enim unde aliquam dignissimos eius hic dicta
+            itaque, dolor nisi! Quasi accusamus laborum voluptate facilis
+            tempore mollitia, totam blanditiis sapiente quae, non debitis odio.
+            Doloremque excepturi voluptate ad aliquid quam accusamus aut.
+          </p>
         </div>
+        <img className="aboutImg" src={about} alt="" />
       </div>
 
-      <div>
-        <h2 className="text-center mt-5">Our Team</h2>
-        <div className="container mt-5">
-          <div className="row g-4">
-            {OurTeam.map((props) => {
-              return (
-                <div
-                  key={props.id}
-                  className="cardContainer col-lg-3 col-md-4 col-sm-12"
-                >
-                  <div className="card border-warning bg-dark text-light">
-                    <img src={props.image} class="card-img-top" alt="" />
-                    <div className="card-body d-flex justify-content-around">
-                      <h3 className="card-title">{props.title}</h3>
-                      <p className="card-text">{props.description}</p>
-                      <a href={props.link} className="icon">
-                        <i className="fa fa-github fa-2x icon-spacing"></i>
+      <div className="container mt-5 ">
+        <h2 className="text-center py-5">Our Team</h2>
+        <div className="row g-4">
+          {OurTeam.map((props) => {
+            return (
+              <div
+                key={props.id}
+                className="cardContainer col-lg-3 col-md-4 col-sm-12"
+              >
+                <div className="card border-warning bg-dark text-light">
+                  <img src={props.image} class="card-img-top" alt="" />
+                  <div className="card-body d-flex justify-content-around">
+                    <h3 className="card-title">{props.title}</h3>
+                    <div className="icon-container align-items-center">
+                      <a href={props.github} className="icon ">
+                        <i className="fa fa-github fa-lg"></i>
+                      </a>
+                      <a href={props.linkedin} className="icon">
+                        <i className="fa fa-linkedin fa-lg"></i>
                       </a>
                     </div>
                   </div>
                 </div>
-              );
-            })}
-          </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </section>
